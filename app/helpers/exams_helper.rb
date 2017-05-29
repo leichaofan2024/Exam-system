2ee7f2428374270938bfd202fc7bb3859c7d9de4
cinly <cinlyma@gmail.com>
2017-05-29 11:07:06 +0800

    fix navbar's name

--- app/helpers/exams_helper.rb
+++ app/helpers/exams_helper.rb
@@ -1,2 +1,55 @@
 module ExamsHelper
+
+    def render_a1_status(exam)
+      if exam.is_a1en
+        content_tag(:span, "", :class => "fa fa-check btn btn-xs btn-success")
+      else
+        content_tag(:span, "", :class => "fa fa-square-o btn btn-xs btn-warning")
+      end
+    end
+
+
+    def render_b1_status(exam)
+      if exam.is_b1en
+        content_tag(:span, "", :class => "fa fa-check btn btn-xs btn-success")
+      else
+        content_tag(:span, "", :class => "fa fa-square-o btn btn-xs btn-warning")
+      end
+    end
+
+    def render_c1_status(exam)
+      if exam.is_c1en
+        content_tag(:span, "", :class => "fa fa-check  btn btn-xs btn-success")
+      else
+        content_tag(:span, "", :class => "fa fa-square-o btn btn-xs btn-warning")
+      end
+    end
+
+    def render_d1_status(exam)
+      if exam.is_d1en
+        content_tag(:span, "", :class => "fa fa-check  btn btn-xs btn-success")
+      else
+        content_tag(:span, "", :class => "fa fa-square-o btn btn-xs btn-warning")
+      end
+    end
+
+
+
+    def render_e1_status(exam)
+      if exam.is_e1en
+        content_tag(:span, "", :class => "fa fa-check btn btn-xs btn-success")
+      else
+        content_tag(:span, "", :class => "fa fa-square-o btn btn-xs btn-warning")
+      end
+    end
+
+    def render_f1_status(exam)
+      if exam.is_f1en
+        content_tag(:span, "", :class => "fa fa-check btn btn-xs btn-success")
+      else
+        content_tag(:span, "", :class => "fa fa-square-o btn btn-xs btn-warning")
+      end
+    end
+
+
 end
