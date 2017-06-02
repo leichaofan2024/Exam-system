6ef36235af8be1ef570734c00d937f8b84c9b97c
cinly <cinlyma@gmail.com>
2017-06-02 18:19:37 +0800

    change seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -34,79 +34,37 @@ create_quizzes =
 end
 
 
-
+for i in 1..18 do
 create_questions =
   Question.create!([content: "1+1=？",answer_1:"2", answer_2:"1",answer_3:"3",answer_4:"4" ,
-    quiz_id: 1, is_a1en: true ,is_b1en: false,is_c1en: false,is_d1en: false])
+    quiz_id: i, is_a1en: true ,is_b1en: false,is_c1en: false,is_d1en: false])
 
 create_questions =
   Question.create!([content: "category什么意思？",answer_1:"类", answer_2:"类别",answer_3:"不知道",answer_4:"好",
-    quiz_id: 1, is_a1en: false ,is_b1en: true,is_c1en: false,is_d1en: false])
+    quiz_id: i, is_a1en: false ,is_b1en: true,is_c1en: false,is_d1en: false])
 
 create_questions =
   Question.create!([content: "要如何背单词？",answer_1:"背诵", answer_2:"书写",answer_3:"默念",answer_4:"hello",
-    quiz_id: 1, is_a1en: false ,is_b1en: false,is_c1en: true,is_d1en: false])
+    quiz_id: i, is_a1en: false ,is_b1en: false,is_c1en: true,is_d1en: false])
 
 create_questions =
   Question.create!([content: "false怎么读？",answer_1:"不会", answer_2:"没答案",answer_3:"乱写的",answer_4:"问google",
-     quiz_id: 1, is_a1en: false ,is_b1en: false,is_c1en: false,is_d1en: true])
+     quiz_id: i, is_a1en: false ,is_b1en: false,is_c1en: false,is_d1en: true])
 
 create_questions =
  Question.create!([content: "1+3=？",answer_1:"1", answer_2:"2",answer_3:"4",answer_4:"5",
-    quiz_id: 1, is_a1en: false ,is_b1en: false,is_c1en: false,is_d1en: true])
+    quiz_id: i, is_a1en: false ,is_b1en: false,is_c1en: false,is_d1en: true])
 
 create_questions =
   Question.create!([content: "你好用英语怎么说",answer_1:"hello", answer_2:"hi",answer_3:"good",answer_4:"fine" ,
-    quiz_id: 1, is_a1en: true ,is_b1en: false,is_c1en: false,is_d1en: false])
+    quiz_id: i, is_a1en: true ,is_b1en: false,is_c1en: false,is_d1en: false])
 
 
 create_questions =
   Question.create!([content: "乱写的题目？",answer_1:"第一类", answer_2:"第二类",answer_3:"第三类",answer_4:"第四类",
-    quiz_id: 1, is_a1en: false ,is_b1en: true,is_c1en: false,is_d1en: false])
+    quiz_id: i, is_a1en: false ,is_b1en: true,is_c1en: false,is_d1en: false])
 
 create_questions =
   Question.create!([content: "努力学习？",answer_1:"规范一", answer_2:"规范二",answer_3:"规范三",answer_4:"规范四",
-    quiz_id: 1, is_a1en: false ,is_b1en: false,is_c1en: true,is_d1en: false])
-
-
-
-    create_questions =
-      Question.create!([content: "false怎么读？",answer_1:"不会", answer_2:"没答案",answer_3:"乱写的",answer_4:"问google",
-         quiz_id: 2, is_a1en: false ,is_b1en: false,is_c1en: false,is_d1en: true])
-
-    create_questions =
-     Question.create!([content: "1+3=？",answer_1:"1", answer_2:"2",answer_3:"4",answer_4:"5",
-        quiz_id: 2, is_a1en: false ,is_b1en: false,is_c1en: false,is_d1en: true])
-
-    create_questions =
-      Question.create!([content: "你好用英语怎么说",answer_1:"hello", answer_2:"hi",answer_3:"good",answer_4:"fine" ,
-        quiz_id: 2, is_a1en: true ,is_b1en: false,is_c1en: false,is_d1en: false])
-
-
-    create_questions =
-      Question.create!([content: "乱写的题目？",answer_1:"第一类", answer_2:"第二类",answer_3:"第三类",answer_4:"第四类",
-        quiz_id: 2, is_a1en: false ,is_b1en: true,is_c1en: false,is_d1en: false])
-
-    create_questions =
-      Question.create!([content: "努力学习？",answer_1:"规范一", answer_2:"规范二",answer_3:"规范三",answer_4:"规范四",
-        quiz_id: 2, is_a1en: false ,is_b1en: false,is_c1en: true,is_d1en: false])
-
-        create_questions =
-          Question.create!([content: "1+1=？",answer_1:"2", answer_2:"1",answer_3:"3",answer_4:"4" ,
-            quiz_id: 3, is_a1en: true ,is_b1en: false,is_c1en: false,is_d1en: false])
-
-        create_questions =
-          Question.create!([content: "category什么意思？",answer_1:"类", answer_2:"类别",answer_3:"不知道",answer_4:"好",
-            quiz_id: 3, is_a1en: false ,is_b1en: true,is_c1en: false,is_d1en: false])
-
-        create_questions =
-          Question.create!([content: "要如何背单词？",answer_1:"背诵", answer_2:"书写",answer_3:"默念",answer_4:"hello",
-            quiz_id: 3, is_a1en: false ,is_b1en: false,is_c1en: true,is_d1en: false])
-
-        create_questions =
-          Question.create!([content: "false怎么读？",answer_1:"不会", answer_2:"没答案",answer_3:"乱写的",answer_4:"问google",
-             quiz_id: 3, is_a1en: false ,is_b1en: false,is_c1en: false,is_d1en: true])
-
-        create_questions =
-         Question.create!([content: "1+3=？",answer_1:"1", answer_2:"2",answer_3:"4",answer_4:"5",
-            quiz_id: 3, is_a1en: false ,is_b1en: false,is_c1en: false,is_d1en: true])
+    quiz_id: i, is_a1en: false ,is_b1en: false,is_c1en: true,is_d1en: false])
+end 
